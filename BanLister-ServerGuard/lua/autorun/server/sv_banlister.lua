95cdc99c2f18461b8dba6109931b6d8470bd3e99
Charlie <charliesimon02@gmail.com>
2018-08-26 01:09:16 +1000

    Initial

---
+++ BanLister-ServerGuard/lua/autorun/server/sv_banlister.lua
@@ -0,0 +1,44 @@
+BanLister = BanLister or {}
+
+BanLister.MaxBans = 15 -- If the user has equal to or more recorded bans then this value, then the user will be kicked from the server.
+BanLister.RangeBans = "month" -- "total" - "month" -- MaxBans Time period.
+BanLister.KickHim = false -- if false, instead of kicking users for MaxBans a message will be sent to staff. If true a message won't be sent and the user will be kicked.
+BanLister.KickReason = "Sorry %s but this server is protected by Ban Lister ensuring a safe community" -- %s = player name
+BanLister.AdminMessage = "%s has %d bans in the past month recorded" -- %s = player name
+
+--DO NOT MODIFY PAST THIS POINT--
+
+hook.Add("PlayerInitialSpawn", "BanLister.CheckForBans", function(ply)
+	local steamid64 = ply:SteamID64()
+
+	local s = BanLister.RangeBans == "month" and "retrieve-month" or "retrieve"
+
+	http.Fetch(
+		string.format("https://api.banlister.com/%s.php?steamid=%s", s, steamid64),
+		function(body, size, h, code)
+			local data = util.JSONToTable(body)
+
+			local count = table.Count(data)
+
+			PrintTable(data)
+
+			print("Bans count: "..count)
+
+			if count >= BanLister.MaxBans then return end
+
+			local name = ply.SteamName and ply:SteamName() or ply:Name()
+			if BanLister.KickHim then
+				ply:Kick(string.format(BanLister.KickReason, name))
+			else
+				for k,v in pairs(player.GetAll()) do
+					if not v:IsAdmin() then continue end
+
+					v:ChatAddText(Color(220, 200, 0), "[BanLister] ", color_white, string.format(BanLister.AdminMessage, name, count))
+				end
+			end
+		end,
+		function(e)
+			print("[BanLister] API FAILED WITH STATUS: "..e)
+		end
+	)
+end)
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Gmod-ServerGuard
+# Ban Lister
+
+Ban Lister gathers bans from trusted secure servers and compiles a free to use public repository.
+
+This plugin is connected to BanLister's ever-growing repository, allowing you full control over your user's fate when they don't meet your standards.
+
+### Installation
+
+Download the ZIP file and extract ```BanLister-ServerGuard``` addon to the addons folder.
+
+### Configuration
+
+Open ```sv_banlister.lua``` from the ```lua/autorun/server``` directory and modify the following fields as follows.
+```
+BanLister.MaxBans = 15 -- If the user has equal to or more recorded bans then this value, then the user will be kicked from the server.
+BanLister.RangeBans = "month" -- "total" - "month" -- MaxBans Time period.
+BanLister.KickHim = false -- if false, instead of kicking users for MaxBans a message will be sent to staff. If true a message won't be sent and the user will be kicked.
+BanLister.KickReason = "Sorry %s but this server is protected by Ban Lister ensuring a safe community" -- %s = player name
+BanLister.AdminMessage = "%s has %d bans in the past month recorded" -- %s = player name
+```
+
+### Security
+
+Our developers are committed to ensuring a full working and secure plugin. This plugin connects securely to our database to retrieving data when necessary. If you have further safety concerns or general questions you're more then welcome to come and chat to a developer https://discord.gg/F7FH2nn
